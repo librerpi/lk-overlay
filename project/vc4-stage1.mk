72ce3be5a9251e4b6f8155b6a4a53320e8ff59cc
Michael Bishop <cleverca22@gmail.com>
2021-09-06 13:27:58 -0300

    fix deps for vc4-stage1

--- project/vc4-stage1.mk
+++ project/vc4-stage1.mk
@@ -6,6 +6,7 @@ MODULES += \
 	app/vc4-stage1 \
 	platform/bcm28xx/otp \
 	platform/bcm28xx/rpi-ddr2 \
+	platform/bcm28xx/sdhost \
 
 GLOBAL_DEFINES += BOOTCODE=1 WITH_NO_FP=1 NOVM_MAX_ARENAS=2 NOVM_DEFAULT_ARENA=0
 BOOTCODE := 1
