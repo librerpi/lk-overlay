8c33168d77aae68add05ffb6d7281c5974ea8f27
Michael Bishop <cleverca22@gmail.com>
2022-09-17 10:39:05 -0300

    fix a small bug

--- app/sleepy/sleepy.c
+++ app/sleepy/sleepy.c
@@ -5,7 +5,7 @@
 #include <lk/reg.h>
 
 static void sleepy_entry(const struct app_descriptor *app, void *args) {
-  *REG32(PM_RSTS) = 0; // clear reset reason, so it runs partition 0 on reboot
+  *REG32(PM_RSTS) = PM_PASSWORD | 0; // clear reset reason, so it runs partition 0 on reboot
   thread_sleep(2 * 60 * 1000); // 2 minutes
   platform_halt(HALT_ACTION_REBOOT, HALT_REASON_SW_RESET);
 }
