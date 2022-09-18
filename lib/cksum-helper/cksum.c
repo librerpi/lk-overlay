dff9b7fa2216b8d346336dfe2ebeb6587816c626
Michael Bishop <cleverca22@gmail.com>
2022-09-18 08:58:24 -0300

    implement testing all sums in a file

--- lib/cksum-helper/cksum.c
+++ lib/cksum-helper/cksum.c
@@ -1,4 +1,6 @@
 #include <app.h>
+#include <assert.h>
+#include <lib/cbuf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,6 +25,12 @@ typedef struct {
   void (*update)(void *context, const void *data, int length);
   const uint8_t *(*finalize)(void *context);
 } hash_algo_implementation;
+
+typedef struct {
+  filehandle *fh;
+  int offset;
+  cbuf_t buf;
+} file_buffer;
 
 #ifdef WITH_LIB_MINCRYPT
 static void sha256_init(void *context) {
@@ -62,6 +70,14 @@ void print_hash(const uint8_t *hash, int hash_size) {
   puts("");
 }
 
+void print_hash_to_string(const uint8_t *hash, int hash_size, char *outbuf) {
+  int i;
+  for (i=0; i<hash_size; i++) {
+    sprintf(outbuf + (2*i), "%02x", hash[i]);
+  }
+  outbuf[2*i] = 0;
+}
+
 void test_hash_algo(const char *name, const void *data, int size, const char *expected) {
   const hash_algo_implementation *algo = get_implementation(name);
   if (!algo) {
@@ -83,33 +99,133 @@ void test_hash_algo(const char *name, const void *data, int size, const char *ex
     printf("%s hashes dont match, %s != %s\n", name, expected, hash_str);
   }
   free(context);
+  free(hash_str);
 }
 
 #ifdef WITH_LIB_FS
-void hash_file(const char *path, const hash_algo_implementation *algo, uint8_t *hash) {
+status_t hash_file(const char *path, const hash_algo_implementation *algo, uint8_t *hash) {
   filehandle *fh;
   status_t ret;
 
   ret = fs_open_file(path, &fh);
+  if (ret != 0) {
+    return ret;
+  }
   void *context = malloc(algo->context_size);
   algo->init(context);
   void *buffer = malloc(1024);
   int offset = 0;
   while ((ret = fs_read_file(fh, buffer, offset, 1024)) > 0) {
-    printf("read %d bytes\n", ret);
+    //printf("read %d bytes\n", ret);
     offset += ret;
     algo->update(context, buffer, ret);
   }
   memcpy(hash, algo->finalize(context), algo->hash_size);
   free(context);
   free(buffer);
+
+  return 0;
 }
 
-bool verify_hashes(const hash_algo_implementation *algo, const char *prefix, const char *sums_file) {
+void filebuf_init(file_buffer *buf, filehandle *fh) {
+  buf->fh = fh;
+  buf->offset = 0;
+  cbuf_initialize(&buf->buf, 2048);
+}
+
+void paths_join(char *dest, const char *a, const char *b) {
+  dest[0] = 0;
+  strlcat(dest, a, FS_MAX_PATH_LEN);
+  strlcat(dest, "/", FS_MAX_PATH_LEN);
+  strlcat(dest, b, FS_MAX_PATH_LEN);
+}
+
+/* *
+ * reads until a \n is found, or maxlen is hit
+ * if a \n is found, it remains in the output buffer, and is followed by a \0
+ * if a \n is NOT found, and either EOF or maxlen is reached, the buffer is not null terminated
+ *
+ * returns the chars read
+ * */
+int filebuf_readline(file_buffer *buf, char *buffer, int maxlen) {
+  int ret;
+  int pos = 0;
+  bool eof = false;
+  while (!eof) {
+    if (cbuf_space_used(&buf->buf) == 0) {
+      uint8_t *buffer2 = malloc(1024);
+      ret = fs_read_file(buf->fh, buffer2, buf->offset, 1024);
+      assert(ret >= 0); // TODO, error handling
+      if (ret == 0) {
+        eof = true;
+      } else {
+        ret = cbuf_write(&buf->buf, buffer2, ret, false);
+        buf->offset += ret;
+      }
+      free(buffer2);
+    }
+    while (cbuf_space_used(&buf->buf) > 0) {
+      ret = cbuf_read_char(&buf->buf, &buffer[pos], false);
+      if (buffer[pos] == '\n') {
+        pos++;
+        buffer[pos] = '\0';
+        return pos+1;
+      } else if (pos == maxlen) {
+        return pos+1;
+      } else {
+        pos++;
+      }
+    }
+  }
+  return pos;
+}
+
+bool verify_hashes(const hash_algo_implementation *algo, const char *prefix, const char *sums_file, int *matches, int *mismatches, int *failure) {
   filehandle *fh;
   status_t ret;
+  char *pathbuf = malloc(FS_MAX_PATH_LEN);
+  void *actual_hash = malloc(algo->hash_size);
+  char *actual_hash_str = malloc((algo->hash_size*2)+1);
 
-  ret = fs_open_file(path, &fh);
+  paths_join(pathbuf, prefix, sums_file);
+
+  ret = fs_open_file(pathbuf, &fh);
+  assert(ret == 0);
+
+  file_buffer buf;
+  filebuf_init(&buf, fh);
+  const int linebuf_size = 64+2+FS_MAX_PATH_LEN+2;
+  char *linebuf = malloc(linebuf_size);
+  while ((ret = filebuf_readline(&buf, linebuf, linebuf_size-1)) > 0) {
+    linebuf[ret] = 0;
+    char *hash = strtok(linebuf, " ");
+    char *name = strtok(NULL, "\n");
+    while (name[0] == ' ') name++;
+    //printf("hash: %s, name: '%s'\n", hash, name);
+    paths_join(pathbuf, prefix, name);
+    //printf("hash: %s, name: %s\n", hash, pathbuf);
+    ret = hash_file(pathbuf, algo, actual_hash);
+    if (ret != 0) {
+      printf("%s: failure to open/read\n", name);
+      (*failure)++;
+      continue;
+    }
+    print_hash_to_string(actual_hash, algo->hash_size, actual_hash_str);
+    //printf("actual hash: %s\n", actual_hash_str);
+
+    if (strcmp(hash, actual_hash_str) == 0) {
+      //printf("%s: OK\n", name);
+      (*matches)++;
+    } else {
+      printf("%s: FAILED\n", name);
+      (*mismatches)++;
+    }
+  }
+
+  free(pathbuf);
+  free(actual_hash_str);
+  free(actual_hash);
+  free(linebuf);
   return false;
 }
 #endif
@@ -148,8 +264,17 @@ static void helper_entry(const struct app_descriptor *app, void *args) {
     printf("mount failure: %d\n", ret);
     return;
   }
+  if (0) {
+    const hash_algo_implementation *algo = get_implementation("sha256");
+    int matches=0, mismatches=0, failure=0;
+    verify_hashes(algo, "/test/", "everything.sums", &matches, &mismatches, &failure);
+    printf("%d matches, %d mismatches, %d failure\n", matches, mismatches, failure);
+  }
   const hash_algo_implementation *algo = get_implementation("sha256");
-  verify_hashes(algo, "/test/", "everything.sums");
+  void *hash = malloc(algo->hash_size);
+  ret = hash_file("/test/./lk/external/platform/nrfx/mdk/nrf5340_xxaa_network.ld", algo, hash);
+  //assert(ret == 0);
+  dump_thread(get_current_thread());
 }
 
 APP_START(helper_test)
