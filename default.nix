945b8b6203d87f5b3b7970ff771a59f2a43c44b5
Michael Bishop <cleverca22@gmail.com>
2023-11-01 15:15:12 -0300

    missed a file

--- default.nix
+++ default.nix
@@ -101,6 +101,7 @@ in lib.fix (self: {
     rpi3.start = vc4.callPackage ./lk.nix { project = "rpi3-start"; };
     rpi4.recovery = vc4.callPackage ./lk.nix { project = "rpi4-recovery"; };
     rpi4.start4 = vc4.callPackage ./lk.nix { project = "rpi4-start4"; };
+    stage1-bad-apple = vc4.callPackage ./lk.nix { project = "stage1-bad-apple"; };
     vc4.stage1 = vc4.callPackage ./lk.nix { project = "vc4-stage1"; };
     vc4.stage2 = vc4.callPackage ./lk.nix {
       project = "vc4-stage2";
