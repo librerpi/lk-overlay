45f2eb3c2075df68705ecba5308dde07d68f1744
Michael Bishop <cleverca22@gmail.com>
2025-11-30 12:13:32 -0400

    print uptime relative to binary start

--- platform/bcm28xx/print_timestamp.c
+++ platform/bcm28xx/print_timestamp.c
@@ -1,3 +1,4 @@
+#include <arch/arch_ops.h>
 #include <lk/reg.h>
 #include <platform/bcm28xx.h>
 #include <platform/bcm28xx/clock.h>
@@ -6,6 +7,10 @@
 
 void print_timestamp() {
   uint32_t clock_lo = *REG32(ST_CLO);
+
+#if 1
+  clock_lo -= arch_init_timestamp;
+#endif
 
   printf("%3d.%06d ", clock_lo / 1000000, clock_lo % 1000000);
 }
