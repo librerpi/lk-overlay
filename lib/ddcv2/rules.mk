82b4420e63fe84dace2d8277407eba1c0371aa01
Michael Bishop <cleverca22@gmail.com>
2026-06-30 01:38:47 -0300

    add i2c to the deps of the ddc module

--- lib/ddcv2/rules.mk
+++ lib/ddcv2/rules.mk
@@ -2,7 +2,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 GLOBAL_INCLUDES += $(LOCAL_DIR)/include/
 MODULE := $(LOCAL_DIR)
-MODULES += lib/edid
+MODULES += lib/edid dev/bcm-i2c
 MODULE_SRCS += $(LOCAL_DIR)/ddc.c
 
 include make/module.mk
