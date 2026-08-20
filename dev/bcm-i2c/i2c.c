22367891c9e13f1ce790d86ba6d9e72a38107ab1
Michael Bishop <cleverca22@gmail.com>
2026-08-20 03:07:17 -0300

    add i2c debug commands

--- dev/bcm-i2c/i2c.c
+++ dev/bcm-i2c/i2c.c
@@ -1,11 +1,14 @@
-#include <lib/heap.h>
 #include <dev/bcm-i2c.h>
+#include <dev/gpio.h>
 #include <lib/edid.h>
+#include <lib/heap.h>
 #include <lib/hexdump.h>
 #include <lib/video_timing.h>
+#include <lk/console_cmd.h>
 #include <lk/debug.h>
 #include <lk/init.h>
 #include <platform/bcm28xx.h>
+#include <platform/bcm28xx/gpio.h>
 #include <platform/bcm28xx/udelay.h>
 #include <string.h>
 
@@ -39,6 +42,103 @@ typedef struct {
 #define I2C_STATUS_TXD      BV(4)
 #define I2C_STATUS_DONE     BV(1)
 
+static int cmd_show_mux(int argc, const console_cmd_args *argv);
+static int cmd_mux_set(int argc, const console_cmd_args *argv);
+static int cmd_i2c_detect(int argc, const console_cmd_args *argv);
+static int cmd_smbus_read(int argc, const console_cmd_args *argv);
+static void i2c_detect(int nr, int start, int end);
+
+STATIC_COMMAND_START
+STATIC_COMMAND("i2c_showmux", "show i2c pinmux", &cmd_show_mux)
+STATIC_COMMAND("i2c_mux", "mux i2c to a given set of pins", &cmd_mux_set)
+STATIC_COMMAND("i2cdetect", "do an i2cdetect pass", &cmd_i2c_detect)
+STATIC_COMMAND("smbus_read", "do an smbus read", &cmd_smbus_read)
+STATIC_COMMAND_END(i2c);
+
+static int cmd_show_mux(int argc, const console_cmd_args *argv) {
+  for (int i=0; i<54; i++) {
+    int config = gpio_get_config(i);
+    switch (i) {
+    case 0:
+    case 28:
+      if (config == kBCM2708Pinmux_ALT0) printf("GPIO%02d: %s\n", i, "SDA0");
+      break;
+    case 1:
+    case 29:
+      if (config == kBCM2708Pinmux_ALT0) printf("GPIO%02d: %s\n", i, "SCL0");
+      break;
+    case 2:
+      if (config == kBCM2708Pinmux_ALT0) printf("GPIO%02d: %s\n", i, "SDA1");
+      break;
+    case 3:
+      if (config == kBCM2708Pinmux_ALT0) printf("GPIO%02d: %s\n", i, "SCL1");
+      break;
+    case 44:
+      if (config == kBCM2708Pinmux_ALT1) printf("GPIO%02d: %s\n", i, "SDA0");
+      if (config == kBCM2708Pinmux_ALT2) printf("GPIO%02d: %s\n", i, "SDA1");
+      break;
+    case 45:
+      if (config == kBCM2708Pinmux_ALT1) printf("GPIO%02d: %s\n", i, "SCL0");
+      if (config == kBCM2708Pinmux_ALT2) printf("GPIO%02d: %s\n", i, "SCL1");
+      break;
+    case 46:
+      if (config == kBCM2708Pinmux_ALT0) printf("GPIO%02d: %s\n", i, "SDA0");
+      if (config == kBCM2708Pinmux_ALT1) printf("GPIO%02d: %s\n", i, "SDA1");
+      break;
+    case 47:
+      if (config == kBCM2708Pinmux_ALT0) printf("GPIO%02d: %s\n", i, "SCL0");
+      if (config == kBCM2708Pinmux_ALT1) printf("GPIO%02d: %s\n", i, "SCL1");
+      break;
+    }
+  }
+  return 0;
+}
+
+static int cmd_mux_set(int argc, const console_cmd_args *argv) {
+  if (argc != 3) {
+    printf("usage: i2c_mux <controller_nr> <basepin>\n");
+    return 0;
+  }
+  int controller = argv[1].u;
+  int basepin = argv[2].u;
+  if ((controller == 0) && (basepin == 0)) {
+    gpio_config(0, kBCM2708Pinmux_ALT0);
+    gpio_config(1, kBCM2708Pinmux_ALT0);
+  } else if ((controller == 1) && (basepin == 2)) {
+    gpio_config(2, kBCM2708Pinmux_ALT0);
+    gpio_config(3, kBCM2708Pinmux_ALT0);
+  } else if ((controller == 0) && (basepin == 28)) {
+    gpio_config(28, kBCM2708Pinmux_ALT0);
+    gpio_config(29, kBCM2708Pinmux_ALT0);
+  } else if ((controller == 0) && (basepin == 44)) {
+    gpio_config(44, kBCM2708Pinmux_ALT1);
+    gpio_config(45, kBCM2708Pinmux_ALT1);
+  } else if ((controller == 1) && (basepin == 44)) {
+    gpio_config(44, kBCM2708Pinmux_ALT2);
+    gpio_config(45, kBCM2708Pinmux_ALT2);
+  } else if ((controller == 0) && (basepin == 46)) {
+    gpio_config(46, kBCM2708Pinmux_ALT0);
+    gpio_config(47, kBCM2708Pinmux_ALT0);
+  } else if ((controller == 1) && (basepin == 46)) {
+    gpio_config(46, kBCM2708Pinmux_ALT1);
+    gpio_config(47, kBCM2708Pinmux_ALT1);
+  } else {
+    printf("invalid pin chosen\n");
+  }
+  return 0;
+}
+
+static int cmd_i2c_detect(int argc, const console_cmd_args *argv) {
+  int controller = 0;
+  int start = 0;
+  int end = 0x78;
+  if (argc >= 2) controller = argv[1].u;
+  if (argc >= 3) start = argv[2].u;
+  if (argc >= 4) end = argv[3].u;
+  i2c_detect(controller, start, end);
+  return 0;
+}
+
 i2cController *getController(int nr) {
   switch (nr) {
   case 0:
@@ -50,6 +150,22 @@ i2cController *getController(int nr) {
     return (i2cController*)BSC2_BASE;
   }
   panic("invalid i2c controller");
+}
+
+static int cmd_smbus_read(int argc, const console_cmd_args *argv) {
+  int controller = argv[1].u;
+  int addr = argv[2].u;
+  int reg = argv[3].u;
+  int size = argv[4].u;
+  uint8_t *buffer = malloc(size);
+  smbus_read(controller, addr, reg, buffer, size);
+  for (int i=0; i<size; i++) {
+    printf("%02x ", buffer[i]);
+    if ((i % 16) == 15) puts("");
+  }
+  puts("");
+  free(buffer);
+  return 0;
 }
 
 static void init_i2c_controller(int nr) {
